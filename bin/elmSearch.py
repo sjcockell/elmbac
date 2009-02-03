8194619077084c748e02e27b886e090226b506ec
sjcockell <sjcockell@gmail.com>
2009-02-03 17:41:51 +0000

    Adding methods for pretty print, and retrieving all results committer - sjcockell@gmail.com  Changes to be committed:
    	modified:   bin/elmSearch.py

--- bin/elmSearch.py
+++ bin/elmSearch.py
@@ -6,18 +6,53 @@ def main(f):
 	s = Sequences.Sequences(f)
 	seqs = s.sequenceList
 	elms = e.elmList
-	test(seqs, elms)
+#	test(seqs, elms)
+	results = getResultSet(seqs, elms)
+	printResultSet(results)
 
 def test(s, e):
 	k1 = s.keys()
 	k2 = e.keys()
-	elm = k2[0]
 	seq = k1[0]
-	elmPat = e[elm]
 	seqSeq = s[seq]
-	print elmPat, seqSeq
-	m = Match.Match(seqSeq, elmPat)
-	print m.getMatches()
+	print seq, seqSeq
+	for elm in k2:
+		elmPat = e[elm]
+		m = Match.Match(seqSeq, elmPat)
+		if m.getMatches():
+			print elm, elmPat
+			print m.getMatches()
+			print m.getNumberMatches()
+
+def getResultSet(sequences, elmMotifs):
+	resultArray = []
+	sequenceNames = sequences.keys()
+	elmNames = elmMotifs.keys()
+	listTitles = elmNames[:]
+	listTitles.insert(0, '')
+	tupleTitles = tuple(listTitles)
+	resultArray.append(tupleTitles)
+	i = 1
+	for sequenceName in sequenceNames:
+		tempList = []
+		sequence = sequences[sequenceName]
+		tempList.append(sequenceName)
+		for elmName in elmNames:
+			elmMotif = elmMotifs[elmName]
+			matcher = Match.Match(sequence, elmMotif)
+			tempList.append(matcher.getNumberMatches())
+		tempTuple = tuple(tempList)
+		resultArray.append(tempTuple)
+		i += 1
+	return resultArray
+
+def printResultSet(r):
+	fh = open('outfile', 'w')
+	for line in r:
+		for element in line:
+			print str(element)+"\t",
+		print
+	close(fh)
 
 if __name__ == '__main__':
 	try:
