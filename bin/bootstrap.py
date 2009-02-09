36312a5b50f76182e2d619d60d5e9eb0e0f3c02d
sjcockell <sjcockell@gmail.com>
2009-02-09 17:33:09 +0000

    Adding methods for statistical analysis Committer - sjcockell@gmail.com  Changes to be committed:
    	new file:   bin/bootstrap.py
    	modified:   elm/Sequences.py

---
+++ bin/bootstrap.py
@@ -0,0 +1,88 @@
+from elm import ELM, Sequences, Match
+import sys
+from random import randrange
+from rpy import *
+
+def main(f):
+	e = ELM.ELM()
+	s = Sequences.Sequences(f)
+	seqs = s.sequenceList
+	s.setIndexedSequenceLists()
+	list = s.getIndexedSequenceList()
+	elms = e.elmList
+#	test(seqs, elms)
+	totalScores = []
+	avScores = []
+	for i in range(0, 1000):
+		randomList = getRandomSequences(list, 25)
+		score = scoreSequences(randomList, elms['LIG_TRAF2_1'])
+		totalScores.append(score)
+	#print r.wilcox_test(totalScores)
+#	r.library('moments')
+#	r.kurtosis(totalScores)
+#	r.skewness(totalScores)
+	ttestResults = r.t_test(totalScores, conf_level=0.99)
+	mannWhitneyResults = r.wilcox_test(totalScores, conf_int='TRUE' ,conf_level=0.99)
+	summary = r.summary(totalScores)
+#	ttestResults = r.t_test(avScores, conf_level=0.99)
+#	mannWhitneyResults = r.wilcox_test(avScores, conf_int='TRUE' ,conf_level=0.99)
+	print "Mean:\t"+str(summary['Mean'])
+	print "T Conf:\t"+str(ttestResults['conf.int'])
+	print "MW Conf:\t"+str(mannWhitneyResults['conf.int'])
+	testList = getRandomSequences(list, 25)
+	testScore = scoreSequences(randomList, elms['LIG_TRAF2_1'])
+#	testAvScore = float(testScore)/10.0
+	print testScore
+	#	print str(k)+':'+str(summary[k])
+	#for k in ttestResults.keys():
+	#	print str(k)+':'+str(ttestResults[k])
+	#for k in mannWhitneyResults.keys():
+	#	print str(k)+':'+str(mannWhitneyResults[k])
+
+
+def getRandomSequences(seqList, number):
+	start = 0
+	end = len(seqList)
+	randList = []
+	for i in range(0, number):
+		rand = randrange(start, end)
+		randList.append(seqList[rand])
+	return randList
+
+def scoreSequences(list, elm):
+	score = 0
+	scoreArray = []
+	for seq in list:
+		m = Match.Match(seq, elm)
+		scoreArray.append(m.getNumberMatches())
+		score = score + m.getNumberMatches()
+	print scoreArray
+	return score
+
+def test(s, e):
+	k1 = s.keys()
+	k2 = e.keys()
+	seq = k1[0]
+	seqSeq = s[seq]
+	print seq, seqSeq
+	for elm in k2:
+		elmPat = e[elm]
+		m = Match.Match(seqSeq, elmPat)
+		if m.getMatches():
+			print elm, elmPat
+			print m.getMatches()
+			print m.getNumberMatches()
+
+
+if __name__ == '__main__':
+	try:
+		organism = sys.argv[1]
+	except IndexError:
+		print 'Usage: python elmSearch.py organismName'
+		print 'Where viable names are:'
+		print 'ecoli'
+	else:
+		print organism
+		file = organism + '.fa'
+		main(file)
+
--- elm/Sequences.py
+++ elm/Sequences.py
@@ -1,9 +1,21 @@
 from Bio import SeqIO
 class Sequences:
 	sequenceList = {}
+	indexedSequenceIDList = []
+	indexedSequenceList = []
+	
 	def __init__(self, list):
 		h = open('data/'+list, 'r')
 		for seq_record in SeqIO.parse(h, "fasta"):
 			self.sequenceList[seq_record.id] = str(seq_record.seq)
 
-		
+	def setIndexedSequenceLists(self):
+		i = 0
+		keys = self.sequenceList.keys() 
+		for key in keys:
+			self.indexedSequenceIDList.append(key)
+			self.indexedSequenceList.append(self.sequenceList[key])
+			i += 1
+	
+	def getIndexedSequenceList(self):
+		return self.indexedSequenceList
